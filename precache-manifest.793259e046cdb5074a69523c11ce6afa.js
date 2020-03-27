cae0981f4377a07566820e044f4aa0791fe0e32d
Carlo Alberto Degli Atti <lordkada@gmail.com>
2020-03-27 10:42:40 +0100

    Updates

---
+++ precache-manifest.793259e046cdb5074a69523c11ce6afa.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b9b13261470bd72abc6bd3d93ba6b0b7",
+    "url": "/website/index.html"
+  },
+  {
+    "revision": "994c58f06d1d2a68cab3",
+    "url": "/website/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "def79ea28d97d810c635",
+    "url": "/website/static/js/2.c2fa3d69.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/website/static/js/2.c2fa3d69.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "994c58f06d1d2a68cab3",
+    "url": "/website/static/js/main.e751eb4a.chunk.js"
+  },
+  {
+    "revision": "c0e7a96e1e1fafc71966",
+    "url": "/website/static/js/runtime-main.c0bafae9.js"
+  },
+  {
+    "revision": "5d5d9eefa31e5e13a6610d9fa7a283bb",
+    "url": "/website/static/media/logo.5d5d9eef.svg"
+  }
+]);
