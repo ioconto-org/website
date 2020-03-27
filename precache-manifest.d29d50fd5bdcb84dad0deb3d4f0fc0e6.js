e26c00b817cfb8dbaef4ee3063aeb41737429243
Carlo Alberto Degli Atti <lordkada@gmail.com>
2020-03-27 11:36:03 +0100

    Updates

---
+++ precache-manifest.d29d50fd5bdcb84dad0deb3d4f0fc0e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "5c31024fd784eb23684b56fa1f034185",
+    "url": "/index.html"
+  },
+  {
+    "revision": "b7a01a275eac710e93de",
+    "url": "/static/css/main.5f361e03.chunk.css"
+  },
+  {
+    "revision": "fe83273aef9a4cb84e46",
+    "url": "/static/js/2.45dc77ba.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.45dc77ba.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "b7a01a275eac710e93de",
+    "url": "/static/js/main.52ee4e0e.chunk.js"
+  },
+  {
+    "revision": "caf2a04453df4bace4e4",
+    "url": "/static/js/runtime-main.009173a0.js"
+  }
+]);
--- service-worker.js
+++ service-worker.js
@@ -14,7 +14,7 @@
 importScripts("https://storage.googleapis.com/workbox-cdn/releases/4.3.1/workbox-sw.js");
 
 importScripts(
-  "/website/precache-manifest.ae1b0cced3b7fec9991bbca5ebe8aa08.js"
+  "/precache-manifest.d29d50fd5bdcb84dad0deb3d4f0fc0e6.js"
 );
 
 self.addEventListener('message', (event) => {
@@ -33,7 +33,7 @@ workbox.core.clientsClaim();
 self.__precacheManifest = [].concat(self.__precacheManifest || []);
 workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
 
-workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/website/index.html"), {
+workbox.routing.registerNavigationRoute(workbox.precaching.getCacheKeyForURL("/index.html"), {
   
   blacklist: [/^\/_/,/\/[^\/?]+\.[^\/]+$/],
 });
