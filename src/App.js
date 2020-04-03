c0d39ff0e9e1fb6a08f0f25be743c518869c7e33
Carlo Alberto Degli Atti <lordkada@gmail.com>
2020-04-03 15:49:32 +0200

    Replace link to ioconto-org/datasets repository

--- src/App.js
+++ src/App.js
@@ -12,7 +12,7 @@ function App() {
                         In Italia e nel Mondo abbiamo di fronte un futuro incerto.<br />
 Le decisioni critiche che lo influenzeranno in senso positivo devono essere basate sulla analisi di dati completi, verificati e che siano stati raccolti in modo omogeneo, relativamente alla diffusione delle infezioni, ospedalizzazioni e decessi causati dal virus SARS-Cov-2.<br />
 IoConto vuole fornire, a tutti gli enti e anche a singoli individui, un sistema semplice da usare, efficiente, pubblico per raccogliere i dati, normalizzarli e distribuirli a chiunque ne abbia bisogno per disegnare scenari, prendere decisioni oculate e informare.<br />
-<br /><a href="https://github.com/ioconto/covid19">Uniti ce la faremo</a>.
+<br /><a href="https://github.com/ioconto-org/datasets">Uniti ce la faremo</a>.
                         </p>
                         <p>
                             #IoConto
