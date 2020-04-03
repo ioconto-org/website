fc5c208a7c9b72062093a55aa161c8dc54e6e910
Marco Ottolini <marco@ottolini.com>
2020-04-03 10:21:20 +0200

    added mission

--- src/App.js
+++ src/App.js
@@ -8,6 +8,12 @@ function App() {
             <Switch>
                 <Route path="/">
                     <header className="App-header">
+                        <p className="mission">
+                        In Italia e nel Mondo abbiamo di fronte un futuro incerto.<br />
+Le decisioni critiche che lo influenzeranno in senso positivo devono essere basate sulla analisi di dati completi, verificati e che siano stati raccolti in modo omogeneo, relativamente alla diffusione delle infezioni, ospedalizzazioni e decessi causati dal virus SARS-Cov-2.<br />
+IoConto vuole fornire, a tutti gli enti e anche a singoli individui, un sistema semplice da usare, efficiente, pubblico per raccogliere i dati, normalizzarli e distribuirli a chiunque ne abbia bisogno per disegnare scenari, prendere decisioni oculate e informare.<br />
+<br /><a href="https://github.com/ioconto/covid19">Uniti ce la faremo</a>.
+                        </p>
                         <p>
                             #IoConto
                         </p>
