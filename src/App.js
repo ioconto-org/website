664c47e63fb3b795b5b6288efb030d7189f8988b
Carlo Alberto Degli Atti <lordkada@gmail.com>
2020-04-06 18:21:10 +0200

    Add bootstrap and make layout mobile friendly

--- src/App.js
+++ src/App.js
@@ -1,35 +1,64 @@
 import React from 'react';
-import './App.css';
+import './custom.scss';
 import {BrowserRouter as Router, Route, Switch} from "react-router-dom";
+import {Button, Col, Container, Nav, Row} from "react-bootstrap";
 
 function App() {
     return (
         <Router>
             <Switch>
                 <Route path="/">
-                    <header className="App-header">
-                        <div className="content">
-                        <div className="col-6">
-                            <p className="mission">
-                            In Italia e nel Mondo abbiamo di fronte un futuro incerto.<br />
+                    <Container className={"p-3"}>
+                        <Row>
+                            <Col lg={6}>
+                                <h1>
+                                    #IoConto
+                                </h1>
+                                <p>
+                                    In Italia e nel Mondo abbiamo di fronte un futuro incerto.
+                                    <br/>
+                                    Perché questo futuro possa essere indirizzato in senso positivo, è necessario che si
+                                    prendano ora decisioni basate sulla analisi di dati completi, verificati e che siano
+                                    stati raccolti in modo omogeneo relativamente
+                                    alla diffusione delle infezioni, ospedalizzazioni e decessi causati dal virus
+                                    SARS-Cov-2.
+                                    <br/>
+                                    IoConto vuole fornire, a tutti gli enti e anche a singoli individui, un sistema
+                                    semplice da usare, efficiente, pubblico per raccogliere i dati, normalizzarli e
+                                    distribuirli a chiunque ne abbia bisogno al fine di disegnare scenari, prendere
+                                    decisioni oculate e informare. <a href="https://github.com/ioconto/covid19">Uniti ce
+                                    la faremo.</a>
+                                </p>
+                                <Nav.Link
+                                    href="https://docs.google.com/forms/d/e/1FAIpQLSe_gJ1vM_2WXYkCNoyGwWRjLJcSlvEf9DwZAfpwR3IXLqbwSw/viewform">
+                                    <Button variant="primary" size={"sm"} block>Segnalare Dati Comunali</Button>
+                                </Nav.Link>
+                                <Nav.Link
+                                    href="https://docs.google.com/forms/d/e/1FAIpQLSfbs90muj6Fe30pb2fi12kZDznBrvYhPSnB8nmSWjWP58jKuA/viewform">
+                                    <Button variant="primary" size={"sm"} block>Segnalare Dati Ospedalieri</Button>
+                                </Nav.Link>
+                                <Nav.Link
+                                    href="https://docs.google.com/forms/d/e/1FAIpQLSd6_80C6IPNAyyMrIM5Z6MoUMtmTiEi56Ex9H_4rFvkgp9CvQ/viewform">
+                                    <Button variant="primary" size={"sm"} block>Segnalare Voglio Partecipare</Button>
+                                </Nav.Link>
+                            </Col>
+                            <Col sm={6}>
+                                <iframe width="100%" height="100%"
+                                        frameBorder="0"
+                                        allowFullScreen
+                                        title="IoConto.org"
+                                        src="https://umap.openstreetmap.fr/en/map/morti-marzo-2020_440940#7/44.308/10.750?scaleControl=true&miniMap=false&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false">
 
-Perché questo futuro possa essere indirizzato in senso positivo, è necessario che si prendano ora decisioni basate sulla analisi di dati completi, verificati e che siano stati raccolti in modo omogeneo relativamente
-alla diffusione delle infezioni, ospedalizzazioni e decessi causati dal virus SARS-Cov-2.<br />
-IoConto vuole fornire, a tutti gli enti e anche a singoli individui, un sistema semplice da usare, efficiente, pubblico per raccogliere i dati, normalizzarli e distribuirli a chiunque ne abbia bisogno al fine di disegnare scenari, prendere decisioni oculate e informare. 
-    <br /><a href="https://github.com/ioconto/covid19">Uniti ce la faremo</a>.
-                            </p>
-                            <p className="mission">
-                                #IoConto
-                            </p>
-                            <a className="button" href="https://docs.google.com/forms/d/e/1FAIpQLSe_gJ1vM_2WXYkCNoyGwWRjLJcSlvEf9DwZAfpwR3IXLqbwSw/viewform">Segnalare Dati Comunali</a>
-                            <a className="button" href="https://docs.google.com/forms/d/e/1FAIpQLSfbs90muj6Fe30pb2fi12kZDznBrvYhPSnB8nmSWjWP58jKuA/viewform">Segnalare Dati Ospedalieri</a>
-                            <a className="button" href="https://docs.google.com/forms/d/e/1FAIpQLSd6_80C6IPNAyyMrIM5Z6MoUMtmTiEi56Ex9H_4rFvkgp9CvQ/viewform">Voglio Partecipare</a>
-                        </div>
-                        <div className="col-6">
-                            <iframe width="100%" height="100%" frameborder="0" allowfullscreen src="https://umap.openstreetmap.fr/en/map/morti-marzo-2020_440940#7/44.308/10.750?scaleControl=true&miniMap=false&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="//umap.openstreetmap.fr/en/map/morti-marzo-2020_440940#7/44.308/10.750">Visualizzazione a schermo intero</a></p>
-                        </div>
-                        </div>
-                    </header>
+                                </iframe>
+                                <p className={"text-center"}>
+                                    <Nav.Link
+                                        href="//umap.openstreetmap.fr/en/map/morti-marzo-2020_440940#7/44.308/10.750">
+                                        <small>Visualizzazione a schermo intero</small>
+                                    </Nav.Link>
+                                </p>
+                            </Col>
+                        </Row>
+                    </Container>
                 </Route>
             </Switch>
         </Router>
