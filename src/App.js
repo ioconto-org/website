92d51ff570cd3e72e027cabf7f863bd372bec6f2
Marco Ottolini <marco@ottolini.com>
2020-04-06 13:37:48 +0200

    better support for sharing

--- src/App.js
+++ src/App.js
@@ -12,9 +12,11 @@ function App() {
                         <div className="col-6">
                             <p className="mission">
                             In Italia e nel Mondo abbiamo di fronte un futuro incerto.<br />
-    Le decisioni critiche che lo influenzeranno in senso positivo devono essere basate sulla analisi di dati completi, verificati e che siano stati raccolti in modo omogeneo, relativamente alla diffusione delle infezioni, ospedalizzazioni e decessi causati dal virus SARS-Cov-2.<br />
-    IoConto vuole fornire, a tutti gli enti e anche a singoli individui, un sistema semplice da usare, efficiente, pubblico per raccogliere i dati, normalizzarli e distribuirli a chiunque ne abbia bisogno per disegnare scenari, prendere decisioni oculate e informare.<br />
-    <br /><a href="https://github.com/ioconto-org/datasets">Uniti ce la faremo</a>.
+
+Perché questo futuro possa essere indirizzato in senso positivo, è necessario che si prendano ora decisioni basate sulla analisi di dati completi, verificati e che siano stati raccolti in modo omogeneo relativamente
+alla diffusione delle infezioni, ospedalizzazioni e decessi causati dal virus SARS-Cov-2.<br />
+IoConto vuole fornire, a tutti gli enti e anche a singoli individui, un sistema semplice da usare, efficiente, pubblico per raccogliere i dati, normalizzarli e distribuirli a chiunque ne abbia bisogno al fine di disegnare scenari, prendere decisioni oculate e informare. 
+    <br /><a href="https://github.com/ioconto/covid19">Uniti ce la faremo</a>.
                             </p>
                             <p className="mission">
                                 #IoConto
@@ -24,7 +26,7 @@ function App() {
                             <a className="button" href="https://docs.google.com/forms/d/e/1FAIpQLSd6_80C6IPNAyyMrIM5Z6MoUMtmTiEi56Ex9H_4rFvkgp9CvQ/viewform">Voglio Partecipare</a>
                         </div>
                         <div className="col-6">
-                            <iframe width="100%" height="100%" frameborder="0" allowfullscreen src="//umap.openstreetmap.fr/en/map/morti-marzo-2020_440940#7/41.796/12.415?scaleControl=true&miniMap=false&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="//umap.openstreetmap.fr/en/map/morti-marzo-2020_440940">Visualizzazione a schermo intero</a></p>
+                            <iframe width="100%" height="100%" frameborder="0" allowfullscreen src="https://umap.openstreetmap.fr/en/map/morti-marzo-2020_440940#7/44.308/10.750?scaleControl=true&miniMap=false&scrollWheelZoom=true&zoomControl=true&allowEdit=false&moreControl=true&searchControl=null&tilelayersControl=null&embedControl=null&datalayersControl=true&onLoadPanel=undefined&captionBar=false"></iframe><p><a href="//umap.openstreetmap.fr/en/map/morti-marzo-2020_440940#7/44.308/10.750">Visualizzazione a schermo intero</a></p>
                         </div>
                         </div>
                     </header>
